46834b0fbbe424e19f456cc73c7e30fd4174d94f
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-02-04 08:54:15 -0300

    feat: show input textin p tag

--- assets/scripts/index.scripts.js
+++ assets/scripts/index.scripts.js
@@ -1,5 +1,4 @@
 function exibirNome(){
     let nome = document.getElementById("nome").value;
-
-    console.log("Nome digitado: ", nome);
-}
+    document.getElementById("msg").innerHTML = nome;
+};
